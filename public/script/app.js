0728afda993a97ad1a54cbf9a09f0bea46817938
Angel Manuel Góez Giraldo <angelmanuel.goez@hotmail.es>
2019-06-22 22:24:49 -0500

    Counter app

--- public/script/app.js
+++ public/script/app.js
@@ -1,18 +1,146 @@
 'use strict';
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        'Indecision app'
-    ),
-    React.createElement(
+var user = {
+    name: 'Angel Manuel Góez Giraldo',
+    age: 24,
+    location: 'Medellín - Colombia'
+};
+
+var getLocation = function getLocation(_ref) {
+    var location = _ref.location;
+
+    if (!location) {
+        return 'Unknown';
+    }
+    return React.createElement(
         'p',
         null,
-        'Hi I am basic template, and I changed'
-    )
-);
+        'My location is: ',
+        location
+    );
+};
 
-ReactDOM.render(template, document.getElementById('app'));
+var style = {
+    border: '2px solid #eee',
+    color: 'black',
+    boxShadow: '0px 2px #ccc',
+    padding: '10px',
+    backgroundColor: '#fff',
+    display: 'inline-block',
+    margin: '0px'
+};
+var buttonStyleAdd = {
+    backgroundColor: 'green',
+    color: '#fff',
+    padding: '5px',
+    fontSize: '20px',
+    boxShadow: '1px 2px #ccc',
+    margin: '2px'
+};
+var buttonStyleMinus = {
+    backgroundColor: 'crimson',
+    color: '#fff',
+    padding: '5px',
+    fontSize: '20px',
+    boxShadow: '1px 2px #ccc',
+    margin: '2px'
+};
+var buttonStyleRestart = {
+    backgroundColor: 'blue',
+    color: '#fff',
+    padding: '5px',
+    fontSize: '20px',
+    boxShadow: '1px 2px #ccc',
+    margin: '2px'
+};
+
+var counter = 0;
+
+var addOne = function addOne() {
+    counter++;
+    render();
+};
+var minusOne = function minusOne() {
+    counter--;
+    render();
+};
+var reset = function reset() {
+    counter = 0;
+    render();
+};
+
+var render = function render() {
+
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'div',
+            { style: style },
+            React.createElement(
+                'h1',
+                null,
+                'Indecision app'
+            ),
+            React.createElement(
+                'p',
+                null,
+                'Hi I am basic template, and I changed'
+            ),
+            React.createElement(
+                'ol',
+                null,
+                React.createElement(
+                    'li',
+                    null,
+                    'Item one'
+                ),
+                React.createElement(
+                    'li',
+                    null,
+                    'Item two'
+                )
+            ),
+            getLocation(user)
+        ),
+        React.createElement(
+            'div',
+            { style: style },
+            React.createElement(
+                'h1',
+                null,
+                'Counter mini-app'
+            ),
+            React.createElement(
+                'p',
+                null,
+                'Events and Attributes'
+            ),
+            React.createElement(
+                'p',
+                null,
+                'Counter: ',
+                counter
+            ),
+            React.createElement(
+                'button',
+                { style: buttonStyleAdd, onClick: addOne },
+                'Add 1'
+            ),
+            React.createElement(
+                'button',
+                { style: buttonStyleMinus, onClick: minusOne },
+                'Minus 1'
+            ),
+            React.createElement(
+                'button',
+                { style: buttonStyleRestart, onClick: reset },
+                'Reset'
+            )
+        )
+    );
+
+    ReactDOM.render(template, document.getElementById('app'));
+};
+
+render();
--- src/app.js
+++ src/app.js
@@ -1,8 +1,91 @@
-const template = (
-    <div>
-        <h1>Indecision app</h1>
-        <p>Hi I am basic template, and I changed</p>
-    </div>
+const user = {
+    name:'Angel Manuel Góez Giraldo',
+    age:24,
+    location:'Medellín - Colombia'
+}
+
+const getLocation = ({location}) => {
+    if(!location){
+        return 'Unknown'
+    }
+    return <p>My location is: {location}</p>
+}
+
+let style = {
+    border:'2px solid #eee',
+    color:'black',
+    boxShadow:'0px 2px #ccc',
+    padding:'10px',
+    backgroundColor:'#fff',
+    display: 'inline-block',
+    margin:'0px',
+}
+let buttonStyleAdd= {
+    backgroundColor:'green',
+    color:'#fff',
+    padding:'5px',
+    fontSize:'20px',
+    boxShadow:'1px 2px #ccc',
+    margin:'2px',
+}
+let buttonStyleMinus = {
+    backgroundColor:'crimson',
+    color:'#fff',
+    padding:'5px',
+    fontSize:'20px',
+    boxShadow:'1px 2px #ccc',
+    margin:'2px',
+}
+let buttonStyleRestart = {
+    backgroundColor:'blue',
+    color:'#fff',
+    padding:'5px',
+    fontSize:'20px',
+    boxShadow:'1px 2px #ccc',
+    margin:'2px',
+}
+
+let counter = 0
+
+const addOne = () => {
+    counter++
+    render()
+}
+const minusOne = () => {
+    counter--
+    render()
+}
+const reset = () => {
+    counter = 0
+    render()
+}
+
+const render = () => { 
+
+    const template = (
+        <div>
+            <div style={style}>
+                <h1>Indecision app</h1>
+                <p>Hi I am basic template, and I changed</p>
+                <ol>
+                    <li>Item one</li>
+                    <li>Item two</li>
+                </ol>
+                { getLocation(user) }
+            </div>
+
+            <div style={style}>
+                <h1>Counter mini-app</h1>
+                <p>Events and Attributes</p>
+                <p>Counter: { counter }</p>
+                <button style={buttonStyleAdd} onClick={addOne}>Add 1</button>
+                <button style={buttonStyleMinus} onClick={minusOne}>Minus 1</button>
+                <button style={buttonStyleRestart} onClick={reset}>Reset</button>
+            </div>
+        </div>
     )
 
-ReactDOM.render(template, document.getElementById('app'))
+    ReactDOM.render(template, document.getElementById('app'))
+}
+
+render()
